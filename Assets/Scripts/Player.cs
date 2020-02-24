525b782a2890b294b0a543f05391202ed66eb177
Anton Evstifeev <a.s.evstifeev@gmail.com>
2020-02-24 12:07:04 +0300

    Наброски архитектуры

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour, iShip
+{
+    [SerializeField]
+    private ShipState state;
+
+    public void TakeDamage( int damage )
+    {
+        state.currentHP -= damage;
+    }
+}
---
+++ Assets/Scripts/ShipState.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct ShipState
+{
+    public readonly int maxHP;
+    public int currentHP;
+    public readonly float Speed;
+    public readonly int Power;
+}

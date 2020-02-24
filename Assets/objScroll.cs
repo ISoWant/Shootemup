ab7bb3c5a03101458f86b78d0dcb773e7cdc592e
Arberg <22754697@mail.ru>
2020-02-24 18:18:42 +0300

    upd1

---
+++ Assets/objScroll.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class objScroll : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+
+    private void Update()
+    {
+        transform.position = new Vector3(transform.position.x, transform.position.y + speed, transform.position.z);
+    }
+}
